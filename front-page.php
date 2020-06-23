fb57c98f91b0895becf2588b7837f0d9e629cc01
matthew <matthew@dcooper.co.uk>
2020-06-23 16:45:14 +0100

    internet explorer 10+ warning

--- front-page.php
+++ front-page.php
@@ -5,6 +5,9 @@
 
 get_header();
 ?>
+	<div id="internet_explorer" style="display: none;" >
+		<p>WARNING: You are using an unsupported browser and some things might not work. Please consider using a modern browser, such as <a href="https://www.mozilla.org/en-GB/firefox/new/">Firefox</a>.</p>
+	</div>
 
 	<main id="primary" class="site-main">
 
